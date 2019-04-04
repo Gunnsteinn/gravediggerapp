f0c5a52759cb48e64c771cba68d3fe0ce56cef34
Xprox <guillecolo@gmail.com>
2019-04-04 00:24:40 -0300

    Set Cassandra DB.

--- src/main/java/com/gravediggerapp/GravediggerappApplication.java
+++ src/main/java/com/gravediggerapp/GravediggerappApplication.java
@@ -1,4 +1,4 @@
-package gravediggerapp.example.gravediggerapp;
+package com.gravediggerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
