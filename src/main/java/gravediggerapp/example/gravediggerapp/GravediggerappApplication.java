4685aee26c64d34a657868b5a268e662e88f7fe7
Xprox <guillecolo@gmail.com>
2019-04-03 22:49:36 -0300

    Create a Project.

---
+++ src/main/java/gravediggerapp/example/gravediggerapp/GravediggerappApplication.java
@@ -0,0 +1,13 @@
+package gravediggerapp.example.gravediggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravediggerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GravediggerappApplication.class, args);
+	}
+
+}
